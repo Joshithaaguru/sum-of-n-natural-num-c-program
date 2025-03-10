0182b58b32b8dde5ba7e19b5898c04a77ddfac8a
Joshithaaguru <joshithaaguru@gmail.com>
2025-03-10 22:27:40 +0530

    Add files via upload

---
+++ sum of n natural numbers.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int n ,sum = 0;
+	printf("enter the number:\n");
+	scanf("%d",&n);
+	for(int i=0;i<=n;i++){
+		sum=sum+i;
+	}
+	printf("sum=%d",sum);
+	return 0;
+}
